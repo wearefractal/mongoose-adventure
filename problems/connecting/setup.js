02b66c29a975802fc086904ad5bda62669b5372b
Contra <contra@maricopa.edu>
2013-10-17 01:52:08 -0700

    tracking

--- problems/connecting/setup.js
+++ problems/connecting/setup.js
@@ -1,23 +1,42 @@
+// utils
+var execWrapFile = require.resolve("../../lib/exec-wrap");
+var tracker = require("../../lib/tracker");
+var startMongo = require('../../lib/start-mongo');
+var find = require('../../lib/array-find');
+
+// config
 var path = require('path');
 var dbroot = path.join(__dirname, '../../db');
+var dbString = "mongodb://localhost:9001/workshop";
 
-var startMongo = require('../../lib/startMongo');
-
-var verify = function (cb, env) {
-  console.log(this, env);
+var verify = function (cb) {
+  var data = tracker.get("mongoose");
+  var connectCall = find(data.calls, function(call){
+    return call.fn === "createConnection" && call.args[0] === dbString;
+  });
+  tracker.wipe("mongoose");
+  if (!connectCall) return cb("You either didn't call createConnection or you called it with the wrong arguments");
   cb();
 };
 
 module.exports = function (isRun, cb) {
   var isVerify = !isRun;
 
-  var mongo = startMongo(dbroot, 9001);
+  // wipe old logs
+  tracker.wipe("mongoose");
 
-  return {
-    args: [],
-    stdin: null,
-    long: false,
-    close: mongo.close,
-    verify: verify
-  };
+  var mongo = startMongo(dbroot, 9001);
+  mongo.once('error', cb);
+  mongo.once('ready', function(){
+    cb(null, {
+      args: [],
+      stdin: null,
+      long: false,
+      execWrap: execWrapFile,
+      close: mongo.close,
+      verify: verify
+    });
+  });
 };
+
+module.exports.async = true;
